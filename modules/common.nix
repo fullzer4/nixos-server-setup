eb5cfd131b32d5f243b4f986fa5a4b8bbde1e8e8
fullzer4 <gabrielpelizzaro@gmail.com>
2025-11-02 20:26:04 -0300

    feat: add SSH public key for fullzer4 user

--- modules/common.nix
+++ modules/common.nix
@@ -30,8 +30,8 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
     
-    # TODO: add PUBLIC SSH
     openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCavtjByfE8sq1QjSrWGDr3CXV5l+auae6MMqGu8/qlZHj+w5HxN8IOgQQZf1WB+lIfNCgjyY2vJKtlMxj5jKiiiAdhWZoKKuX2MwmUcWcGD3wqit0UdtkV/PvciMHWCgRzj1miGZ9bjda3hkKtNOC8c1wwHcUJChwxKelMDF9d0uv7SLc9/fK4Yo7ErcFH4dv0pWhyWP224lkczn9rCs+Sb+GdV2VCjTiK7MwniQgv5KI0qhQw41RT0qbfXP3tmZMMOD7iPRBnwN82oDkioLW81/E8z2z/uhw70K+4YV0anFIK73f1o5Jvn4jpwpH8DXzkGktSOEVVDTYsgipneDlk+J2/SvtvzP389zf9gTi6f9TCwBQjWi28hwPxMvXYAU6SHNSA69Z1SfKJ7deUxJwsirUkiGe14syJnWyItyYFP8ReTvqbcU7zMa2s4zcGwY/Viy7M4yxUWb/fLqNI5qzH46PGV9vLQLnO4nnkMHgn9gtyGUasE+tW4gx1HNbEmXHE= gabrielpelizzaro@gmail.com"
     ];
   };
 
